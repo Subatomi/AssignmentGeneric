0b00c58139d2df5520011e93e13ad29fa002b2d4
Subatomi <laderatheo@gmail.com>
2024-03-30 12:15:25 +0800

    some changes

--- src/Arithmetic/Arithmetic.java
+++ src/Arithmetic/Arithmetic.java
@@ -1,35 +1,38 @@
 package Arithmetic;
 
-public class Arithmetic<N extends Number>{
+public class Arithmetic<N> {
     private final N num1;
     private final N num2;
 
-    public Arithmetic(N num1, N num2){
+    public Arithmetic(N num1, N num2) {
+        if (!(num1 instanceof Number) || !(num2 instanceof Number)) {
+            throw new IllegalArgumentException("Both num1 and num2 must be a Number!");
+        }
         this.num1 = num1;
         this.num2 = num2;
     }
 
-    public double add(){
-        return num1.doubleValue() + num2.doubleValue();
+    public double add() {
+        return Double.parseDouble(num1.toString()) +Double.parseDouble(num2.toString());
     }
 
-    public double subtract(){
-        return num1.doubleValue() - num2.doubleValue();
+    public double subtract() {
+        return Double.parseDouble(num1.toString()) - Double.parseDouble(num2.toString());
     }
 
-    public double multiply(){
-        return num1.doubleValue() * num2.doubleValue();
+    public double multiply() {
+        return Double.parseDouble(num1.toString()) * Double.parseDouble(num2.toString());
     }
 
-    public double divide(){
-        return num1.doubleValue() / num2.doubleValue();
+    public double divide() {
+        return Double.parseDouble(num1.toString()) /Double.parseDouble(num2.toString());
     }
 
-    public double getMin(){
-        return Math.max(num1.doubleValue(), num2.doubleValue() );
+    public double getMin() {
+        return Math.min(Double.parseDouble(num1.toString()),Double.parseDouble(num2.toString()));
     }
 
-    public double getMax(){
-        return Math.min(num1.doubleValue(), num2.doubleValue() );
+    public double getMax() {
+        return Math.max(Double.parseDouble(num1.toString()),Double.parseDouble(num2.toString()));
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,32 @@
 import Arithmetic.Arithmetic;
 import Map.MyMap;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-//        Arithmetic p = new Arithmetic<>(d232,55.332);
-//        System.out.println(p.add());
+        try{
+            Arithmetic p = new Arithmetic<>(55,55.332);
+            System.out.println(p.add());
+            System.out.println(p.getMax());
+            System.out.println(p.getMin());
+        }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
 
-        MyMap map = new MyMap();
-        map.put("anime1", "rezero");
-        map.put("anime2", "code geass");
-        map.put("anime3", "Jojo");
-        System.out.println(map.get("anime3"));
-        map.put("anime3", "Jojo s3");
-        System.out.println(map.get("anime3"));
-        map.remove("anime3");
-        System.out.println(map.get("anime3"));
+
+
+//        MyMap map = new MyMap();
+//        map.put("anime1", "rezero");
+//        map.put("anime2", "code geass");
+//        map.put("anime3", "Jojo");
+//        System.out.println(map.get("anime3"));
+//        map.put("anime3", "Jojo s3");
+//        System.out.println(map.get("anime3"));
+//        map.remove("anime3");
+//        System.out.println(map.get("anime3"));
 
     }
 }
